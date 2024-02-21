b938f1ebb8c66e1959996e3f72b6ca7653a31b92
KITRI <soongon@gmail.com>
2024-02-21 15:06:43 +0900

    initial commit

--- src/main/java/kr/re/kitri/hello/controller/KitriGramController.java
+++ src/main/java/kr/re/kitri/hello/controller/KitriGramController.java
@@ -3,6 +3,8 @@ package kr.re.kitri.hello.controller;
 import kr.re.kitri.hello.model.Article;
 import kr.re.kitri.hello.service.ArticleService;
 import kr.re.kitri.hello.service.impl.ArticleServiceImpl;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -19,41 +21,44 @@ public class KitriGramController {
 
     // 전체 글 보기  GET :: /articles   List<Article>
     @GetMapping("/articles")
-    public List<Article> articleList() {
+    public ResponseEntity<List<Article>> articleList() {
         // 전체글을 조회해서 가져온다...
         // 서비스의 기능을 사용해서 전체글을 조회해서 가져온다.
         List<Article> articleList = articleService.getAllArticles();
 
         // List<Article> 을 리턴한다. --> 자동으로 JSON으로 변환해준다.
-        return articleList;
+        return ResponseEntity.ok(articleList);
     }
 
     // 상세 글 보기  GET :: /articles/{articleId}   Article
     @GetMapping("/articles/{articleId}")
-    public Article articleDetail(@PathVariable Long articleId) {
+    public ResponseEntity<Article> articleDetail(@PathVariable Long articleId) {
         Article article = articleService.getArticleByArticleId(articleId);
         // Article 을 반환
-        return article;
+        return ResponseEntity.ok(article);
     }
     // 글 등록   POST :: /articles
     @PostMapping("/articles")
-    public void registArticle(@RequestBody Article article) {
+    public ResponseEntity<Void> registArticle(@RequestBody Article article) {
         // 프론트엔드에서 전달된 데이터를 확보..
         article.setCreatedAt(LocalDateTime.now());
         // article 객체(데이터가 채워진)를 서비스에 전달해서 기능을 수행..
         articleService.registArticle(article);
+        return ResponseEntity.ok().build();
     }
 
     // 글 수정   PUT :: /articles
     @PutMapping("/articles")
-    public void updateArticle(@RequestBody Article article) {
+    public ResponseEntity<Void> updateArticle(@RequestBody Article article) {
         articleService.updateArticle(article);
+        return ResponseEntity.ok().build();
     }
 
     // 글 삭제   DELETE :: /articles/{articleId}
     @DeleteMapping("/articles/{articleId}")
-    public void removeArticle(@PathVariable Long articleId) {
+    public ResponseEntity<Void> removeArticle(@PathVariable Long articleId) {
         articleService.removeArticle(articleId);
+        return ResponseEntity.ok().build();
     }
 
 }
